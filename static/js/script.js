49696cf06938e74b1f241816229976591cd54a51
RazvanTr10 <>
2024-02-12 20:25:30 +0000

    Add function to collapse navbar.

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+//Collapse navbar function
+
+function collapseNavbar() {
+    const togglebar = document.querySelector('.togglebar')
+    togglebar.style.display = 'flex'
+}
+
+function hideNavbar() {
+    const togglebar = document.querySelector('.togglebar')
+    togglebar.style.display = 'none'
+}
