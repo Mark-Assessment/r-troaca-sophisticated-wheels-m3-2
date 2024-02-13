95c0b2c7b22ce49e143c4eb47694f4a1628f969b
Razvan Troaca <razvantr@yahoo.com>
2024-02-13 20:25:28 +0000

    Add footer and css rules.

--- app.py
+++ app.py
@@ -9,9 +9,19 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 @app.route("/our_fleet")
 def our_fleet():
     return render_template("our_fleet.html")
+
+
+@app.route("/sell_car")
+def sell_car():
+    return render_template("sell_car.html")
 
 
 @app.route("/contact")
