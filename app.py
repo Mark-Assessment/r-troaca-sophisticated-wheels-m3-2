faf546fc71c1b7d1093135de1bbd06541312193c
Razvan Troaca <razvantr@yahoo.com>
2024-02-23 11:26:35 +0000

    Add cards to our_fleet

--- app.py
+++ app.py
@@ -53,8 +53,6 @@ def login():
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                         session["user"] = request.form.get("username").lower()
-                        flash("Welcome, {}".format(
-                            request.form.get("username")))
                         return redirect(url_for(
                             "account", username=session["user"]))
             else:
@@ -99,10 +97,14 @@ def sell_car():
 
 @app.route("/account", methods=["GET", "POST"])
 def account():
-    # grab the session user's username from db
-    username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
-    return render_template("account.html", username=username)
+    # check if "user" key is in the session
+    if "user" in session:
+        # grab the session user's username from db
+        username = mongo.db.users.find_one(
+            {"username": session["user"]})["username"]
+        return render_template("account.html", username=username)
+    else:
+        return render_template("login.html")
 
 
 @app.route("/contact", methods=["GET", "POST"])
